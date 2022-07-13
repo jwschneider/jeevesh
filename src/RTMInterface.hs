00fea4924ca4868bcf9f602cfc514d3ef7b04034
John Schneider <johnwschneider@outlook.com>
2022-07-13 16:22:08 -0500

    add function userAuth

--- src/RTMInterface.hs
+++ src/RTMInterface.hs
@@ -8,8 +8,10 @@ module RTMInterface where
 import RTMTypes
 import Language.Haskell.TH.Syntax
 import qualified Data.Text as T
-
-
+import Web.Browser (openBrowser)
+import Control.Monad.Trans.Either
+import Control.Monad
+import Data.List (foldl')
 
 -- rtmEcho :: T.Text -> Response REcho
 -- rtmEcho name = EitherT $ do
@@ -18,3 +20,17 @@ import qualified Data.Text as T
 $(genRtmMethod "Echo" "rtm.test.echo" "name" ''T.Text ["name"])
 $(genRtmMethod "Frob" "rtm.auth.getFrob" "frob" ''T.Text [])
 
+userAuth :: IO ()
+userAuth = do
+    (key, secret) <- getApiKeyAndSharedSecret
+    frobE <- runEitherT rtmFrob
+    case frobE of 
+        Left error -> print $ err error
+        Right rfrob ->  do 
+            openBrowser $ endpoint ++ "?" ++ 
+                (tail . T.unpack) (foldl' (\b (k,  v) -> b `T.append` "&" `T.append` k `T.append` "=" `T.append` v) T.empty (signRequest params secret))
+            putStrLn "Press any key to continue"
+            void getChar
+            where
+                endpoint = "https://www.rememberthemilk.com/services/auth/"
+                params = [("api_key", key), ("perms", "delete"), ("frob", frob rfrob)]
